5f0956e347cdfd60e1047d9a4644713cb953d295
Saeedullah15 <saeedullahazim65@gmail.com>
2021-03-29 02:20:13 +0600

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require ('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get ('/', (req, res) => {
-    res.send('i know how to open node....yay!!!');
+    const fruit = {
+        name : "apple",
+        weight : "20gm",
+        color : "red"
+    }
+    res.send(fruit);
+})
+
+app.get ('/fruits/banana', (req, res) => {
+    res.send({name : 'banana', price : '50taka', weight : '10gm'});
+})
+
+const users = ["azim", "rahim", "karim", "sabbir", "plabon", "sakeeb"];
+app.get ('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send ({id, name});
+})
+
+//post
+app.post("/adduser", (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 })
 
 app.listen(5000, () => console.log("listening to port 5000"));
